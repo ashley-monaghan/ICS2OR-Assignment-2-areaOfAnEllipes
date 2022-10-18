ec1563cdf52f4df5cf15b19c8edf971ce46f708e
ashley-monaghan <ashley-monaghan@users.noreply.github.com>
2022-10-18 21:47:33 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -6,16 +6,16 @@
 
 "use strict"
 /**
-* This function calculates area of an Ellipse.
-*/
+ * This function calculates area of an Ellipse.
+ */
 function calculate() {
- // input
- const a = parseInt(document.getElementById("a-of-ellipse").value)
- const b = parseInt(document.getElementById("b-of-ellipse").value)
+  // input
+  const a = parseInt(document.getElementById("a-of-ellipse").value)
+  const b = parseInt(document.getElementById("b-of-ellipse").value)
 
- // process
- const area = (b * a) * 3.14
+  // process
+  const area = b * a * 3.14
 
- // output
- document.getElementById("area").innerHTML = "Area is: " + area + " cm²"
+  // output
+  document.getElementById("area").innerHTML = "Area is: " + area + " cm²"
 }
