d7c094a766aac61dfc4b2b7bcd8d6bc15fa4e33d
ashley-monaghan <113056162+ashley-monaghan@users.noreply.github.com>
2022-10-18 17:45:41 -0400

    Code!

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// Copyright (c) 2022 Ashley Monaghan all rights reserved
+//
+// Created by: Ashley Monaghan
+// Created on: Oct 2022
+// This file contains the JS functions for index.html
+
+"use strict"
+/**
+* This function calculates area of an Ellipse.
+*/
+function calculate() {
+ // input
+ const a = parseInt(document.getElementById("a-of-ellipse").value)
+ const b = parseInt(document.getElementById("b-of-ellipse").value)
+
+ // process
+ const area = (b * a) * 3.14
+
+ // output
+ document.getElementById("area").innerHTML = "Area is: " + area + " cm²"
+}
